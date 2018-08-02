7aece6bd2147b6fb9655c5749355b3a6afc08ad2
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-08-02 14:03:32 -0300

    COR-528 View Dash Corr Home VWOD_COR_SUMARIO_VENDA (esert)

--- VIEWS/VWOD_COR_SUMARIO_VENDA.sql
+++ VIEWS/VWOD_COR_SUMARIO_VENDA.sql
@@ -1,9 +1,30 @@
 --------------------------------------------------------
 --  DDL for View VWOD_COR_SUMARIO_VENDA
 --------------------------------------------------------
+/*
+//201808021301 - esert - COR-528
+COR-528:Serviço - Alteração View Dash Corretora Home
+excluir vinculo forcaVenda x corretora
+incluir vinculo venda x corretora
+*/
 
-  CREATE OR REPLACE FORCE VIEW "CORRET"."VWOD_COR_SUMARIO_VENDA" ("CD_EMPRESA", "EMP_DCMS", "TOT_VIDAS", "VALOR_TOTAL", "CD_FORCA_VENDA", "NOME_FORCA", "CPF", "CD_CORRETORA", "NOME_CORRETORA", "CNPJ", "DT_VENDA", "TIPO_VENDA") AS 
-  SELECT ''                                  AS cd_empresa, 
+  CREATE OR REPLACE FORCE VIEW "CORRET"."VWOD_COR_SUMARIO_VENDA" 
+  (
+  "CD_EMPRESA", 
+  "EMP_DCMS", 
+  "TOT_VIDAS", 
+  "VALOR_TOTAL", 
+  "CD_FORCA_VENDA", 
+  "NOME_FORCA", 
+  "CPF", 
+  "CD_CORRETORA", 
+  "NOME_CORRETORA", 
+  "CNPJ", 
+  "DT_VENDA", 
+  "TIPO_VENDA"
+  ) AS 
+  SELECT 
+       ''                                  AS cd_empresa, 
        ''                                  AS emp_dcms, 
        Count(1)                            AS tot_vidas, 
        SUM(p.valor_anual + p.valor_mensal) valor_total, 
@@ -14,8 +35,7 @@
        cor.nome                            AS NOME_CORRETORA, 
        cor.cnpj,
        v.dt_venda,
-       Decode (v.cd_empresa, NULL, 'PF', 
-                             'PME')        AS TIPO_VENDA 
+       Decode (v.cd_empresa, NULL, 'PF', 'PME') AS TIPO_VENDA 
 FROM   tbod_venda v, 
        tbod_venda_vida vv, 
        tbod_empresa emp, 
@@ -25,8 +45,9 @@ FROM   tbod_venda v,
 WHERE  vv.cd_venda = v.cd_venda 
        --and v.cd_forca_vendas is not null 
        AND v.cd_empresa = emp.cd_empresa (+) 
-       AND v.cd_forca_vendas = fv.cd_forca_venda 
-       AND cor.cd_corretora = fv.cd_corretora 
+       AND v.cd_forca_vendas = fv.cd_forca_venda
+       --AND cor.cd_corretora = fv.cd_corretora --//COR-528 - exc
+       AND v.cd_corretora = cor.cd_corretora --//COR-528 - inc
        AND p.cd_plano = v.cd_plano 
 GROUP  BY '', 
           '', 
