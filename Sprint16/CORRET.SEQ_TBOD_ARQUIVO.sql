e1300f3606a2de92660555a4cac90c4f47e29d7f
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-10-24 20:34:37 -0300

    COR-720 SEQ_TBOD_ARQUIVO recriada apenas dev
    com start 17 apos inserts manuais de 12 ate 16.

--- Sprint16/CORRET.SEQ_TBOD_ARQUIVO.sql
+++ Sprint16/CORRET.SEQ_TBOD_ARQUIVO.sql
@@ -11,3 +11,19 @@ NOORDER
 NOCYCLE
 ;
 
+/*
+--201810241207 - esert/jota - seq SEQ_TBOD_ARQUIVO recriada com start 17 apos inserts manuais de 12 ate 16.
+h t t p s : //stackoverflow .com /questions /11671575 /oracle-setting-sequence-to-a-specific-value
+*//*
+DROP SEQUENCE "CORRET"."SEQ_TBOD_ARQUIVO";
+CREATE SEQUENCE "CORRET"."SEQ_TBOD_ARQUIVO" 
+MINVALUE 1 
+MAXVALUE 999999999 
+INCREMENT BY 1 
+START WITH 17 --201810241207 - esert/jota - seq recriada com start 17 apos inserts manuais
+CACHE 20 
+NOORDER 
+NOCYCLE
+;
+*/
+--select SEQ_TBOD_ARQUIVO.CURRVAL from dual;  --201810241207 - esert/jota - nao aceita executar CURRVAL antes de NEXTVAL.
